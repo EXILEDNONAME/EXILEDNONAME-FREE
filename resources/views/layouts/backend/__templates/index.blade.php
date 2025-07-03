38b52d0ea23c1e76e4732ef3f2cfc7a045123c3c
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-07-03 16:30:05 +0700

    6

--- resources/views/layouts/backend/__templates/index.blade.php
+++ resources/views/layouts/backend/__templates/index.blade.php
@@ -7,40 +7,44 @@
 @section('content')
 <div class="row">
   <div class="col-lg-12">
-    <!--begin::Example-->
-    <!--begin::Card-->
     <div class="card card-custom" data-card="true" id="kt_card_1">
       <div class="card-header">
         <div class="card-title">
-          <h3 class="card-label">Card Tools</h3>
+          <h3 class="card-label"> Main </h3>
         </div>
         <div class="card-toolbar">
-          <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="" data-original-title="Toggle Card">
-            <i class="ki ki-arrow-down icon-nm"></i>
-          </a>
-          <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="reload" data-toggle="tooltip" data-placement="top" title="" data-original-title="Reload Card">
-            <i class="ki ki-reload icon-nm"></i>
-          </a>
-          <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary" data-card-tool="remove" data-toggle="tooltip" data-placement="top" title="" data-original-title="Remove Card">
-            <i class="ki ki-close icon-nm"></i>
-          </a>
+          <a href="{{ URL::Current() }}/create" class="btn btn-icon btn-xs btn-hover-light-primary" data-toggle="tooltip" title="{{ __('default.label.create') }}"><i class="fas fa-plus"></i></a>
+          <a id="table-refresh" class="btn btn-icon btn-xs btn-hover-light-primary" data-toggle="tooltip" title="{{ __('default.label.refresh') }}"><i class="fas fa-sync-alt"></i></a>
+          <div data-toggle="collapse" data-target="#collapse-filter" aria-expanded="true"><a class="btn btn-icon btn-xs btn-hover-light-primary" data-toggle="tooltip" title="{{ __('default.label.filter./') }}"><i class="fas fa-filter"></i></a></div>
+          <div class="dropdown dropdown-inline" bis_skin_checked="1">
+            <button type="button" class="btn btn-clean btn-xs btn-icon btn-icon-md" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-download"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right" bis_skin_checked="1">
+              <ul class="navi navi-hover py-5">
+                <li class="navi-item" data-toggle="tooltip" data-original-title="{{ __('default.label.export.description.copy') }}"><a href="javascript:void(0);" class="navi-link" id="export_copy"><i class="navi-icon fa fa-copy"></i> {{ __('default.label.export.copy') }} </a></li>
+                <li class="navi-item" data-toggle="tooltip" data-original-title="{{ __('default.label.export.description.excel') }}"><a href="javascript:void(0);" class="navi-link" id="export_excel"><i class="navi-icon fa fa-file-excel"></i> {{ __('default.label.export.excel') }} </a></li>
+                <li class="navi-item" data-toggle="tooltip" data-original-title="{{ __('default.label.export.description.pdf') }}"><a href="javascript:void(0);" class="navi-link" id="export_pdf"><i class="navi-icon fa fa-file-pdf"></i> {{ __('default.label.export.pdf') }} </a></li>
+                <li class="navi-item" data-toggle="tooltip" data-original-title="{{ __('default.label.export.description.print') }}"><a href="javascript:void(0);" class="navi-link" id="export_print"><i class="navi-icon fa fa-print"></i> {{ __('default.label.export.print') }} </a></li>
+              </ul>
+            </div>
+          </div>
+          <a href="javascript:void(0);" class="btn btn-icon btn-xs btn-hover-light-primary" data-card-tool="toggle"><i class="fas fa-caret-down"></i></a>
         </div>
       </div>
       <div class="card-body">
-
         <div class="table-responsive">
-          <table class="table table-separate table-head-custom table-checkable" id="exilednoname_table">
+          <table class="table table-separate table-head-custom table-checkable table-sm" id="exilednoname_table">
             <thead>
               <tr>
-                <th class="no-export"> </th>
+                <th class="no-export"></th>
                 <th> No. </th>
                 @yield('table-header')
-                <th class="no-export"> </th>
+                <th class="no-export"></th>
               </tr>
             </thead>
           </table>
         </div>
-
       </div>
     </div>
   </div>
@@ -51,58 +55,61 @@
 @push('js')
 <script src="/assets/backend/plugins/custom/datatables/datatables.bundle.js"></script>
 <script>
-  var table = $('#exilednoname_table').DataTable({
-    serverSide: true,
-    searching: true,
-    rowId: 'Collocation',
-    select: {
-      style: 'multi',
-      selector: 'td:first-child .checkable',
+var indexLastColumn = $("#exilednoname_table").find('tr')[0].cells.length-1;
+var table = $('#exilednoname_table').DataTable({
+  scrollX: true,
+    scrollY: true,
+  serverSide: true,
+  searching: true,
+  rowId: 'Collocation',
+  select: {
+    style: 'multi',
+    selector: 'td:first-child .checkable',
+  },
+  ajax: {
+    url: "{{ URL::current() }}",
+  },
+  headerCallback: function(thead, data, start, end, display) {
+    thead.getElementsByTagName('th')[0].innerHTML = `
+    <label class="checkbox checkbox-single checkbox-solid checkbox-primary mb-0">
+    <input type="checkbox" value="" class="group-checkable"/>
+    <span></span>
+    </label>`;
+  },
+  "lengthMenu": [[50, 100, 250, 500, -1], [50, 100, 250, 500, "All"]],
+  columns: [
+    {
+      data: 'checkbox', orderable: false, searchable: false, 'width': '1',
+      render: function(data, type, row, meta) { return '<label class="checkbox checkbox-single checkbox-primary mb-0"><input type="checkbox" data-id="' + row.id + '" class="checkable"><span></span></label>'; },
     },
-    ajax: {
-      url: "{{ URL::current() }}",
+    {
+      data: 'autonumber', orderable: false, searchable: false, 'className': 'align-middle text-center', 'width': '1',
+      render: function(data, type, row, meta) { return meta.row + meta.settings._iDisplayStart + 1; }
     },
-    headerCallback: function(thead, data, start, end, display) {
-      thead.getElementsByTagName('th')[0].innerHTML = `
-      <label class="checkbox checkbox-single checkbox-solid checkbox-primary mb-0">
-        <input type="checkbox" value="" class="group-checkable"/>
-        <span></span>
-      </label>`;
+    @yield('table-body')
+    {
+      data: 'action',
+      orderable: false,
+      searchable: false,
+      'width': '1',
+      render: function(data, type, row) {
+        return '' +
+        '<div class="dropdown dropdown-inline">' +
+        '<button type="button" class="btn btn-hover-light-dark btn-icon btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ki ki-bold-more-ver"></i></button>' +
+        '<div class="dropdown-menu dropdown-menu-xs" style="">' +
+        '<ul class="navi navi-hover py-5">' +
+        '<li class="navi-item"><a href="{{ URL::current() }}/' + row.id + '" class="navi-link"><span class="navi-icon"><i class="flaticon2-expand"></i></span><span class="navi-text">{{ __("default.label.view") }}</span></a></li>' +
+        '<li class="navi-item"><a href="{{ URL::current() }}/' + row.id + '/edit" class="navi-link"><span class="navi-icon"><i class="flaticon2-contract"></i></span><span class="navi-text">{{ __("default.label.edit") }}</span></a></li>' +
+        '<li class="navi-item"><a href="javascript:void(0);" class="navi-link delete" data-id="' + row.id + '"><span class="navi-icon"><i class="flaticon2-trash"></i></span><span class="navi-text"> {{ __("default.label.delete./") }} </span></a></li>' +
+        '</ul>' +
+        '</div>' +
+        '</div>';
+      },
     },
-    "lengthMenu": [[50, 100, 250, 500, -1], [50, 100, 250, 500, "All"]],
-    columns: [
-      {
-        data: 'checkbox', orderable: false, searchable: false, 'width': '1',
-        render: function(data, type, row, meta) { return '<label class="checkbox checkbox-single checkbox-primary mb-0"><input type="checkbox" data-id="' + row.id + '" class="checkable"><span></span></label>'; },
-      },
-      {
-        data: 'autonumber', orderable: false, searchable: false, 'className': 'align-middle text-center', 'width': '1',
-        render: function(data, type, row, meta) { return meta.row + meta.settings._iDisplayStart + 1; }
-      },
-      @yield('table-body')
-      {
-        data: 'action',
-        orderable: false,
-        searchable: false,
-        'width': '1',
-        render: function(data, type, row) {
-          return '' +
-          '<div class="dropdown dropdown-inline">' +
-          '<button type="button" class="btn btn-hover-light-dark btn-icon btn-xs" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ki ki-bold-more-ver"></i></button>' +
-          '<div class="dropdown-menu dropdown-menu-xs" style="">' +
-          '<ul class="navi navi-hover py-5">' +
-          '<li class="navi-item"><a href="{{ URL::current() }}/' + row.id + '" class="navi-link"><span class="navi-icon"><i class="flaticon2-expand"></i></span><span class="navi-text">{{ __("default.label.view") }}</span></a></li>' +
-          '<li class="navi-item"><a href="{{ URL::current() }}/' + row.id + '/edit" class="navi-link"><span class="navi-icon"><i class="flaticon2-contract"></i></span><span class="navi-text">{{ __("default.label.edit") }}</span></a></li>' +
-          '<li class="navi-item"><a href="javascript:void(0);" class="navi-link delete" data-id="' + row.id + '"><span class="navi-icon"><i class="flaticon2-trash"></i></span><span class="navi-text"> {{ __("default.label.delete./") }} </span></a></li>' +
-          '</ul>' +
-          '</div>' +
-          '</div>';
-        },
-      },
-    ],
-    order: [
-      [1, 'asc']
-    ]
-  });
+  ],
+  order: [
+    [indexLastColumn, 'asc']
+  ]
+});
 </script>
 @endpush
